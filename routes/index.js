2cfc2992166630d802b02855939cd2707fa5436b
ikpyohan <ikpyo.han@bntsys.com>
2021-07-21 15:39:20 +0900

    createbutton

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.get('/', (req, res) => {
     var list = template.list(req.list);
     var html = template.HTML(title, list,
         `<h2>${title}</h2><p>${description}</p>`,
-        `<a href="/create">create</a>`
+        ``
     );
     res.send(html);
     });
